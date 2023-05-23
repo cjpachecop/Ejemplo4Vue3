800c5df4a7e6587cec344e36bff9b1036e381dc3
Carlos Julián Pacheco Prada <cjpachecop@ufpso.edu.co>
2023-05-23 12:23:31 -0500

    Ejemplo4Vue3p1

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref, computed } from 'vue'
+const users = [
+{
+  name: 'Jhon',
+  lastname: 'Doe',
+  age: '20'
+},
+{
+  name: 'Jane',
+  lastname: 'Doe',
+  age: '30'
+}
+
+]
+
+const msg = 'Happy'
+const num = ref(0)
+
+num.value = 2
+const computado = computed(function (){
+  return num.value > 0 ? num.value * 2 : 0
+})
+
+const mostrar = () => {
+//alert('Me estan llamando')
+console.log('Me estan llamando')
+
+}
+</script>
+
+<template>
+Hola Mundo {{ msg }}<br>
+valor {{ num }}<br>
+{{ mostrar()}}<br>
+Valor computado {{ computado }}
+<div v-for="(user, index) in users" :key="index">
+ Nombre Completo {{ user.name }} {{  user.lastname}} <br>
+ <span v-if="user.age">
+  Edad: {{ user.age }}
+ </span>
+ 
+</div>
+</template>
+
+<style scoped>
+
+</style>
